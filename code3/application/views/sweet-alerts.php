66afc25f853b8985c632273c5f0a900b9e9848b3
bhushan <bhushank.bwd@gmail.com>
2022-06-29 20:59:57 +0530

    toaster added

--- code3/application/views/sweet-alerts.php
+++ code3/application/views/sweet-alerts.php
@@ -14,16 +14,32 @@
 
 	
 
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
 	<script src="<?= base_url("assets/js/") ?>sweetalert.min.js"></script>
+
+	
+	<script src="<?= base_url("assets/js/") ?>toastr.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?= base_url("assets/css/") ?>toastr.min.css">
+
 </body>
 <script type="text/javascript">
 	$("#button_1").click(function(){
 		// swal("Good job!", "You clicked the button!", "success");
 		// swal("Oh Sorry!", "You clicked the button!", "error");
 		swal("Oh Sorry!", "You clicked the button!", "warning");
+		toastr.success("It is success", 'Success', {
+			position: 'top-center',
+			timeOut: "3000",
+		});
+		toastr.error("It is error", 'Error', {
+			position: 'top-right',
+			timeOut: "3000",
+			positionClass: "toast-bottom-full-width",
+			fadeIn: '300',
+			fadeOut: '1000',
+		});
 	});
 	$("#button_2").click(function(){
 		swal({
