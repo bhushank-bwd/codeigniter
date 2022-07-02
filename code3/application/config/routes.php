e55aefcc7f54f402b47f480cd91b719553b6fac0
bhushan <bhushank.bwd@gmail.com>
2022-07-02 12:46:49 +0530

    check-box select all(code3) 1

--- code3/application/config/routes.php
+++ code3/application/config/routes.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Welcome';
 $route['data-table'] = 'admin/Admin/dataTable';
 $route['get-data-table-data'] = 'admin/Admin/getDataTableData';
+$route['sweet-alerts'] = 'Welcome/sweet';
 $route['check-box'] = 'old/Basic/checkBox';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
